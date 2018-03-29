113685612a2e0c94dbdcb6cb322c5dff31703794
Matthew Wire <devel@mrwire.co.uk>
2018-03-29 17:07:07 +0100

    Add postcode validation function

--- CRM/Civicrmpostcodelookup/Page/Postcode.php
+++ CRM/Civicrmpostcodelookup/Page/Postcode.php
@@ -23,6 +23,8 @@ abstract class CRM_Civicrmpostcodelookup_Page_Postcode extends CRM_Core_Page {
    * @return string
    */
   protected static function format($postcode, $space) {
+    // Convert to uppercase
+    $postcode = strtoupper($postcode);
     // Strip non-alpha characters
     $postcode = preg_replace('/\W/', '', $postcode);
     if (strlen($postcode) > 4) {
@@ -30,6 +32,18 @@ abstract class CRM_Civicrmpostcodelookup_Page_Postcode extends CRM_Core_Page {
       return preg_replace('/^(.*)(\d)(.*)/', "$1{$spacerChar}$2$3", $postcode);
     }
     return $postcode;
+  }
+
+  /**
+   * Is the provided postcode valid?
+   * @param $postcode
+   *
+   * @return bool
+   */
+  protected static function isValidPostcode($postcode) {
+    // Regex provided by UK Gov (https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom)
+    $valid = (boolean)preg_match('/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2})$/', $postcode);
+    return $valid;
   }
 
 }
